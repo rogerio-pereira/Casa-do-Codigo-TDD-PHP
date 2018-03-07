750042e960a1c8e240a3e2b150b6fa1ad22af693
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-07 15:42:55 -0300

    cap 3.2 pg 38 - Refatorando classe ConversorDeNumeroRomano

--- src/CDC/Exemplos/ConversorDeNumeroRomano.php
+++ src/CDC/Exemplos/ConversorDeNumeroRomano.php
@@ -4,11 +4,21 @@ namespace CDC\Exemplos;
 
 class ConversorDeNumeroRomano
 {
+    protected $tabela = [
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'm' => 1000,
+    ];
+
     public function converte($numeroEmRomano)
     {
-        if($numeroEmRomano === 'V')
-            return 5;
+        if(array_key_exists($numeroEmRomano, $this->tabela))
+            return $this->tabela[$numeroEmRomano];
 
-        return 1;
+        return 0;
     }
 }
