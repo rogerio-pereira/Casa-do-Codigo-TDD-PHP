12cc3ca14e6345ef1829b6e0a4b067758d2025fd
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-07 15:37:36 -0300

    cap 3.2 pg 37 - Adicionando teste testeDeveEntenderOSimboloV

--- tests/CDC/Exemplos/ConversorDeNumeroRomanoTest.php
+++ tests/CDC/Exemplos/ConversorDeNumeroRomanoTest.php
@@ -13,4 +13,11 @@ class ConversorDeNumeroRomanoTest extends PHPUnit
         $numero = $romano->converte('I');
         $this->assertEquals(1, $numero);
     }
+
+    public function testDeveReconhecerOSimboloV()
+    {
+        $romano = new ConversorDeNumeroRomano();
+        $numero = $romano->converte('V');
+        $this->assertEquals(5, $numero);
+    }
 }
