c19be12af669dd9533698429b425ad2983e9583f
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-07 17:23:51 -0300

    cap 4.4 - Refatorando Calculadora de Salarios

--- src/CDC/Loja/RH/CalculadoraDeSalarios.php
+++ src/CDC/Loja/RH/CalculadoraDeSalarios.php
@@ -8,13 +8,9 @@ class CalculadoraDeSalarios
 {
     public function calculaSalario(Funcionario $funcionario)
     {
-        if($funcionario->getCargo() === TabelaCargos::DESENVOLVEDOR) {
-            if($funcionario->getSalario() > 3000)
-                return 3200.0;
+        if($funcionario->getSalario() > 3000)
+            return $funcionario->getSalario() * 0.8;
 
-            return 1350.0;
-        }
-
-        return 425.0;
+        return $funcionario->getSalario() * 0.9;
     }
 }
