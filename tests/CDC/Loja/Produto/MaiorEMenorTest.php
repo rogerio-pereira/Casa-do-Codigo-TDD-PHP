04e0dc1c7d79b2ccae2bce0b99d12fe8dae3aa38
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-07 15:31:15 -0300

    Testes MaiorMenor

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -23,4 +23,47 @@ class MaiorEMenorTest extends PHPUnit
         $this->assertEquals('Jogo de Pratos', $maiorMenor->getMenor()->getNome());
         $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
     }
+
+    public function testOrdemCrescente()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto('Jogo de Pratos', 70.00));
+        $carrinho->adiciona(new Produto('Liquidificador', 250.00));
+        $carrinho->adiciona(new Produto('Geladeira', 450.00));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals('Jogo de Pratos', $maiorMenor->getMenor()->getNome());
+        $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
+    }
+
+    public function testOrdemAleatorio()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto('Geladeira', 450.00));
+        $carrinho->adiciona(new Produto('Liquidificador', 250.00));
+        $carrinho->adiciona(new Produto('Jogo de Pratos', 70.00));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals('Jogo de Pratos', $maiorMenor->getMenor()->getNome());
+        $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
+    }
+
+    public function testApenasUmProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto('Geladeira', 450.00));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals('Geladeira', $maiorMenor->getMenor()->getNome());
+        $this->assertEquals('Geladeira', $maiorMenor->getMaior()->getNome());
+    }
 }
