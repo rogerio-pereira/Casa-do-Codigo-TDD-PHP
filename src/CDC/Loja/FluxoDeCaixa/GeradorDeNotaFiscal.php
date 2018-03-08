6f2ce5ddb961256c837bb9a2b0a495cd1691a4d4
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-08 09:32:35 -0300

    Criando Mock de NFDao

--- src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
+++ src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
@@ -8,6 +8,13 @@ use DateTime;
 
 class GeradorDeNotaFiscal
 {
+    private $dao;
+
+    public function __construct(NFDao $dao)
+    {
+        $this->dao = $dao;
+    }
+
     public function gera(Pedido $pedido)
     {
         $nf = new NotaFiscal(
@@ -16,8 +23,7 @@ class GeradorDeNotaFiscal
             new DateTime()
         );
 
-        $nfDao = new NFDao();
-        if($nfDao->persiste($nf))
+        if($this->dao->persiste($nf))
             return $nf;
 
         return null;
--- tests/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscalTest.php
+++ tests/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscalTest.php
@@ -5,16 +5,34 @@ namespace CDC\Loja\FluxoDeCaixa;
 use CDC\Loja\FluxoDeCaixa\GeradorDeNotaFiscal;
 use CDC\Loja\FluxoDeCaixa\Pedido;
 use CDC\Loja\Test\TestCase;
+use Mockery;
 
 class GeradorDeNotaFiscalTest extends TestCase
 {
     public function testDeveGerarNFComValorDeImpostosDescontados()
     {
-        $gerador = new GeradorDeNotaFiscal();
+        $dao = Mockery::mock('CDC\Loja\FluxoDeCaixa\NFDao');
+        $dao->shouldReceive('persiste')->andReturn(true);
+
+        $gerador = new GeradorDeNotaFiscal($dao);
         $pedido = new Pedido("Rogério", 1000, 1);
 
         $nf = $gerador->gera($pedido);
 
         $this->assertEquals(1000 * 0.94, $nf->getValor(), null, 0,00001);
+    }
+
+    public function testDevePersistirNFGerada()
+    {
+        $dao = Mockery::mock('CDC\Loja\FluxoDeCaixa\NFDao');
+        $dao->shouldReceive('persiste')->andReturn(true);
+
+        $gerador = new GeradorDeNotaFiscal($dao);
+        $pedido = new Pedido("Rogério", 1000, 1);
+
+        $nf = $gerador->gera($pedido);
+
+        $this->assertTrue($dao->persiste($nf));
+        $this->assertEquals(1000 * 0.94, $nf->getValor(), null, 0.00001);
     }
 }
