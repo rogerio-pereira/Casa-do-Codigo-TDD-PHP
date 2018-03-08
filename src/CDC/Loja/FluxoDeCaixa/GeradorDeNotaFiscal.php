4a8c9d81cd81c7de38108e6bd14cf2b2ecc296de
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-08 09:09:42 -0300

    Criando regra de negocios para persistir NFDao

--- src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
+++ src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
@@ -2,6 +2,7 @@
 
 namespace CDC\Loja\FluxoDeCaixa;
 
+use CDC\Loja\FluxoDeCaixa\NFDao;
 use CDC\Loja\FluxoDeCaixa\NotaFiscal;
 use DateTime;
 
@@ -9,10 +10,16 @@ class GeradorDeNotaFiscal
 {
     public function gera(Pedido $pedido)
     {
-        return new NotaFiscal(
+        $nf = new NotaFiscal(
             $pedido->getCliente(),
             $pedido->getValorTotal() * 0.94,
             new DateTime()
         );
+
+        $nfDao = new NFDao();
+        if($nfDao->persiste($nf))
+            return $nf;
+
+        return null;
     }
 }
