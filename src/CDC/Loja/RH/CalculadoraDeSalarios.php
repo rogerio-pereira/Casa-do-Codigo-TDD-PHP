4941663a996f3d2c02c65a9ad38f4bfbda57c61c
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-08 08:02:55 -0300

    Testes para calculo de Salario de DBA e TESTADOR, refatoração calculadora

--- src/CDC/Loja/RH/CalculadoraDeSalarios.php
+++ src/CDC/Loja/RH/CalculadoraDeSalarios.php
@@ -9,18 +9,32 @@ class CalculadoraDeSalarios
     public function calculaSalario(Funcionario $funcionario)
     {
         //Desenvolvedores
-        if($funcionario->getCargo() === TabelaCargos::DESENVOLVEDOR) {
-            if($funcionario->getSalario() > 3000)
-                return $funcionario->getSalario() * 0.8;
+        if($funcionario->getCargo() === TabelaCargos::DESENVOLVEDOR)
+            return $this->dezOuVintePorCentoDeDesconto($funcionario);
+        //DBA e TESTADOSR
+        else if(
+            $funcionario->getCargo() === TabelaCargos::DBA ||
+            $funcionario->getCargo() === TabelaCargos::TESTADOR
+        ) 
+            return $this->quinzeOuVinteECincoPorCento($funcionario);
 
-            return $funcionario->getSalario() * 0.9;
-        }
-        //DBA
-        else if($funcionario->getCargo() === TabelaCargos::DBA) {
-            if($funcionario->getSalario() > 2500)
-                return $funcionario->getSalario() * 0.75;
+        throw new Exception("Tipo de funcionario inválido");
+        
+    }
 
+    public function dezOuVintePorCentoDeDesconto($funcionario)
+    {
+        if($funcionario->getSalario() > 3000)
+            return $funcionario->getSalario() * 0.8;
+
+        return $funcionario->getSalario() * 0.9;
+    }
+
+    public function quinzeOuVinteECincoPorCento($funcionario)
+    {
+        if($funcionario->getSalario() < 2500)
             return $funcionario->getSalario() * 0.85;
-        }
+
+        return $funcionario->getSalario() * 0.75;
     }
 }
--- tests/CDC/Loja/RH/CalculadoraDeSalariosTest.php
+++ tests/CDC/Loja/RH/CalculadoraDeSalariosTest.php
@@ -34,10 +34,20 @@ class CalculadoradeSalariosTest extends TestCase
     public function testCalculadoraDeSalarioDBAComSalarioAbaixoDoLimite()
     {
         $calculadora = new CalculadoraDeSalarios();
-        $desenvolvedor = new Funcionario('Rogério', 500.0, TabelaCargos::DBA);
+        $desenvolvedor = new Funcionario('Rogério', 1500.0, TabelaCargos::DBA);
 
         $salario = $calculadora->calculaSalario($desenvolvedor);
 
-        $this->assertEquals(500.0 * 0.85, $salario, null, 0.00001);
+        $this->assertEquals(1500.0 * 0.85, $salario, null, 0.00001);
+    } 
+
+    public function testCalculadoraDeSalarioDBAComSalarioAcimaDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalarios();
+        $desenvolvedor = new Funcionario('Rogério', 4500.0, TabelaCargos::DBA);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(4500.0 * 0.75, $salario, null, 0.00001);
     }   
 }
