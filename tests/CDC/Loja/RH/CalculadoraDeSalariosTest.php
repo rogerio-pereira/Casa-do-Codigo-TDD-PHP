f237ce021f1a6c142293913e1d4ba5c88fd1cd74
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-03-08 08:07:14 -0300

    Adicionando teste para Testadores

--- tests/CDC/Loja/RH/CalculadoraDeSalariosTest.php
+++ tests/CDC/Loja/RH/CalculadoraDeSalariosTest.php
@@ -49,5 +49,25 @@ class CalculadoradeSalariosTest extends TestCase
         $salario = $calculadora->calculaSalario($desenvolvedor);
 
         $this->assertEquals(4500.0 * 0.75, $salario, null, 0.00001);
+    } 
+
+    public function testCalculadoraDeSalarioTestadoresComSalarioAbaixoDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalarios();
+        $desenvolvedor = new Funcionario('Rogério', 1500.0, TabelaCargos::TESTADOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(1500.0 * 0.85, $salario, null, 0.00001);
+    } 
+
+    public function testCalculadoraDeSalarioTestadoresComSalarioAcimaDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalarios();
+        $desenvolvedor = new Funcionario('Rogério', 4500.0, TabelaCargos::TESTADOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(4500.0 * 0.75, $salario, null, 0.00001);
     }   
 }
